aad5f0f6bb1482f9c020a4559e2133b7684e8f2a
Alberto Parziale <alberto@rollstudio.it>
2014-10-25 22:19:24 +0200

    pages + slides no image

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+
+
 (function ($) {
 
 	new WOW().init();
@@ -6,6 +8,30 @@
 		jQuery("#preloader").delay(100).fadeOut("slow");
 		jQuery("#load").delay(100).fadeOut("slow");
 	});
+
+	
+	$(".slide .content").lazyload({
+	    effect : "fadeIn",
+	    threshold : $(window).height()
+	});
+	$(".container_slider").css('height', ($(window).height()*0.9));
+	$(".slide").css('height', ($(window).height()*0.9));
+	$(window).on('resize',function(){
+		$(".container_slider").css('height', ($(window).height()*0.9));
+		$(".slide").css('height', ($(window).height()*0.9));
+	});
+	window.slider = $('.container_slider ul').bxSlider({
+		slideMargin: 0,
+		pager: false,
+		maxSlides: 1,
+		minSlides: 1,
+		infiniteLoop: false,
+		hideControlOnEnd: true,
+		onSlideAfter: function($slideElement, oldIndex, newIndex){
+			$(window).trigger('scroll');
+		}
+	});
+	
 
 
 	//jQuery to collapse the navbar on scroll
@@ -21,10 +47,16 @@
 	$(function() {
 		$('.navbar-nav li a').bind('click', function(event) {
 			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
+			if($anchor.hasClass('dropdown-toggle')){
+				return;
+			}
+			if(!$anchor.hasClass('redirect')){
+				$('html, body').stop().animate({
+					scrollTop: $($anchor.attr('href')).offset().top
+				}, 1500, 'easeInOutExpo');
+				event.preventDefault();
+				$('.navbar-toggle').trigger('click');
+			}
 		});
 		$('.page-scroll a').bind('click', function(event) {
 			var $anchor = $(this);
